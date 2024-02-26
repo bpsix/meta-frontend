002f925cc782fe614fcc2b8ed47b95d27f10dc8b
Brady Six <bpsix646@gmail.com>
2024-02-25 20:51:12 -0800

    remove unnecessary check on zero

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,5 +1,4 @@
 export function getFormattedCurrency(n: number) {
-  if (n === 0) return "+ $0";
   const str = n.toString();
   const parts = str.split(".");
   parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
