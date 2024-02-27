2d202c47a4fb47f9afced0406067b147afe87fe7
Brady Six <bpsix646@gmail.com>
2024-02-26 21:02:57 -0800

    remove coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -7,14 +7,6 @@ const jestConfig = {
   collectCoverageFrom: ["./src/**/*"],
   coverageDirectory: ".coverage/",
   coveragePathIgnorePatterns: ["/node_modules/", "/src/vite-env.d.ts"],
-  coverageThreshold: {
-    global: {
-      branches: 100,
-      functions: 100,
-      lines: 100,
-      statements: 0,
-    },
-  },
 };
 
 export default jestConfig;
