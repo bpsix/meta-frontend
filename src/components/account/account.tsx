7aca6b90e5c9a48a5db23d1eb6a88cd96ac67ab1
Brady Six <bpsix646@gmail.com>
2024-02-27 00:46:50 -0800

    refactor: separate data fetching from parent

--- src/components/account/account.tsx
+++ src/components/account/account.tsx
@@ -1,16 +1,36 @@
+import { useEffect, useState } from "react";
 import { AccountType } from "../../lib/types";
 import { getFormattedCurrency } from "../../lib/util";
 
 import styles from "./acc.module.css";
+import { getAccount } from "../../lib/action";
 
-export default function Account(props: AccountType) {
+export default function AccountWrapper() {
+  const [account, setAccount] = useState<AccountType>({
+    balance: 0,
+    equity: 0,
+    login: 0,
+    profit: 0,
+    server: "?",
+  });
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getAccount();
+        setAccount(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
   return (
     <div className={styles.container}>
-      <div>Login({props.login})</div>
-      <div>Balance({getFormattedCurrency(props.balance)})</div>
-      <div>Equity({getFormattedCurrency(props.equity)})</div>
-      <div>Profit({getFormattedCurrency(props.profit)})</div>
-      <div>Server({props.server})</div>
+      <div>Login({account.login})</div>
+      <div>Balance({getFormattedCurrency(account.balance)})</div>
+      <div>Equity({getFormattedCurrency(account.equity)})</div>
+      <div>Profit({getFormattedCurrency(account.profit)})</div>
+      <div>Server({account.server})</div>
     </div>
   );
 }
